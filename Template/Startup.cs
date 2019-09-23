7e08c5a07a4d36132f7d132912490d8722cf8c7e
Manoj Kumar <manoj.kumar@tenpearls.com>
2019-09-23 11:43:05 +0500

    Initial commit

---
+++ Template/Startup.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+using Template.Api;
+using Template.Api.ActionFilters;
+using Template.Core.Auth;
+using Template.Core.DbContext;
+using Template.Core.Entity;
+using Template.Core.Migrations;
+
+namespace Template
+{
+    public class Startup
+    {
+        private static string SecretKey = Environment.GetEnvironmentVariable("TokenSecretKey");
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AddDependencies(services);
+            AddMVC(services);
+            AddEntityFramework(services);
+            AddSwagger(services);
+            AddJWT(services);
+            services.AddAutoMapper(typeof(Startup));
+           // AddAuthorization(services);
+            //AddHangFire(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Configurations dbMigrationsConfig)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Template API V1");
+            });
+
+            try
+            {
+                dbMigrationsConfig.SeedData().Wait();
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+
+        }
+        private void AddDependencies(IServiceCollection services)
+        {
+            var dependencyInjection = new DependencyInjection();
+            dependencyInjection.Map(services, this.Configuration);
+        }
+
+        private void AddMVC(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(GlobalExceptionAttribute));
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Template",
+                    Description = "Template  Web API",
+                    TermsOfService = "None",
+
+                });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme { In = "header", Description = "Please enter JWT with Bearer into field", Name = "Authorization", Type = "apiKey" });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                { "Bearer", Enumerable.Empty<string>() },
+                 });
+
+                //c.CustomSchemaIds(x => x.FullName);
+            });
+        }
+
+        private void AddJWT(IServiceCollection services)
+        {
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+                options.ValidFor = TimeSpan.FromMinutes(Convert.ToDouble(jwtAppSettingOptions[nameof(JwtIssuerOptions.ValidFor)]));
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+        }
+
+        private void AddEntityFramework(IServiceCollection services)
+        {
+            services.AddDbContext<TemplateContext>(options =>
+                                 options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Scoped);
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+             .AddEntityFrameworkStores<TemplateContext>()
+             .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityCore<ApplicationUser>(x =>
+            {
+                x.Password.RequireDigit = true;
+                x.Password.RequireLowercase = true;
+                x.Password.RequireUppercase = true;
+                x.Password.RequireNonAlphanumeric = true;
+                x.Password.RequiredLength = 8;
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(ApplicationRole), builder.Services);
+            builder.AddEntityFrameworkStores<TemplateContext>().AddDefaultTokenProviders();
+        }
+
+        private void AddAuthorization(IServiceCollection services)
+        {
+            services.AddAuthorization();
+        }
+    }
+}
